4d5c209fbe932e66e68d77d7f5a57ca0233b6993
MartinoGregorio <martinogregorio2@gmail.com>
2025-10-10 16:26:59 +0200

    Disable sourcemap to fix CSP on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,19 @@ export default defineConfig({
   base: '/gregorio-martino-portfolio/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // <-- collega '@' alla cartella src
+      '@': path.resolve(__dirname, 'src'), // collega '@' alla cartella src
+    },
+  },
+  build: {
+    sourcemap: false, // disabilita sourcemap per evitare eval bloccati da CSP
+    minify: 'terser', // minimizza JS per produzione
+    terserOptions: {
+      compress: {
+        // evita eval in fase di compressione
+        global_defs: {
+          "typeof window": "object",
+        },
+      },
     },
   },
 });
