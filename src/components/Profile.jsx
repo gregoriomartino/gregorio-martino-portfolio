09126c0b11d0813a95c658d86fde32353522fd53
Gregorio Martino <martinogregorio2@gmail.com>
2025-11-04 17:46:56 +0100

    fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,7 +3,7 @@ import '../Profile.css'
 export default function Profile({ t }) {
   return (
     <section className="profile-section">
-      <h2 className="profile-title glow">{t.profile}</h2>
+      <h2 className="profile-title ">{t.profile}</h2>
       <p className="profile-text">{t.profileText}</p>
     </section>
   )
